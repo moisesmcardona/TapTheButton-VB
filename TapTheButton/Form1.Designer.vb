d69dc7b7ce62ffed47cc83755636592f298eedf0
Moises Cardona <moises@moises-studios.com>
2018-01-19 12:06:14 -0500

    Add project files.

---
+++ TapTheButton/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(38, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(836, 442)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "TAP!!!!!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(201, 471)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(191, 55)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Taps: 0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 536)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(155, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Developed by: Moisés Cardona"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(863, 536)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(28, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "v1.0"
+        '
+        'Timer1
+        '
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Black
+        Me.Label4.Location = New System.Drawing.Point(479, 471)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(218, 55)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Time: 30"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(769, 471)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(105, 43)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Restart"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(216, 524)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(155, 25)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "High Score: 0"
+        Me.Label5.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(903, 558)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Tap The Button"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ TapTheButton/Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+    Private Tap As Long = 0
+    Private TimeLabelUpdate As Integer = 30
+    Private StartCount As Integer = 0
+    Private Sub UpdateScore()
+        If My.Settings.HighScore < Tap Then
+            My.Settings.HighScore = Tap
+            My.Settings.Save()
+            Label5.Text = "High Score: " & My.Settings.HighScore
+            Label5.Visible = True
+        End If
+    End Sub
+    Private Sub LoadScore()
+        If My.Settings.HighScore = 0 = False Then
+            Label5.Text = "High Score: " & My.Settings.HighScore
+            Label5.Visible = True
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If StartCount = 0 Then
+            Tap = 0
+            If Label4.Text = "Time's Up" Then Label4.Text = "Time: 30"
+            Label1.Text = "Taps: 0"
+            Label1.Update()
+            Timer1.Interval = 1000
+            Timer1.Enabled = True
+            Timer1.Start()
+            Dim AudioStream As System.IO.Stream = My.Resources.Start
+            My.Computer.Audio.Play(AudioStream, AudioPlayMode.Background)
+            StartCount = 1
+        End If
+        Tap = Tap + 1
+        Label1.Text = "Taps: " & Tap
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LoadScore()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        TimeLabelUpdate = TimeLabelUpdate - 1
+        Label4.Text = "Time: " & TimeLabelUpdate
+        If Label4.Text = "Time: 0" Then
+            StartCount = 0
+            Label4.Text = "Time's Up"
+            TimeLabelUpdate = 30
+            Timer1.Stop()
+            Button1.Enabled = False
+            Dim StopStream As System.IO.Stream = My.Resources._Stop
+            My.Computer.Audio.Play(StopStream, AudioPlayMode.Background)
+            UpdateScore()
+        End If
+        If TimeLabelUpdate <= 10 Then
+            If Label4.ForeColor = Color.Black Then Label4.ForeColor = Color.Red Else Label4.ForeColor = Color.Black
+        End If
+        Dim WarningStream As System.IO.Stream = My.Resources.Warning
+        Dim FiveSecondsCountdownStream As System.IO.Stream = My.Resources.FiveSecondCountdown
+        If TimeLabelUpdate = 15 Then My.Computer.Audio.Play(WarningStream, AudioPlayMode.Background)
+        If TimeLabelUpdate = 6 Then My.Computer.Audio.Play(FiveSecondsCountdownStream, AudioPlayMode.Background)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Button1.Enabled = True
+        If Label4.Text = "Time's Up" Then Label4.Text = "Time: 30"
+    End Sub
+End Class
